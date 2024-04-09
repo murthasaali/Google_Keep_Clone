f0fb31e0b456a09e5c18556b1fe196afe7954d62
murthasaali <murthasaalick123@gmail.com>
2024-04-09 20:19:14 +0530

    sidebar updated

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -6,7 +6,7 @@ import bell from "../assets/notification.png"
 function SideBar() {
   return (
     <div className="md:w-72 md:h-3/4 w-full h-20 absolute z-[100] md:flex  justify-end items-center  md:left-0  md:top-20 bottom-0 ">
-      <div className="md:w-[90%] w-full h-full rounded-lg bg-stone-100 bg-opacity-40 flex justify-center   gap-5 items-center md:flex-col flex-row">
+      <div className="md:w-[90%] w-full h-full rounded-lg bg-stone-100 bg-opacity-40 flex md:justify-center justify-evenly   gap-5 items-center md:flex-col flex-row">
         <button
           className=" bg-black h-10 flex justify-center items-center rounded-full w-10  relative"
         >
