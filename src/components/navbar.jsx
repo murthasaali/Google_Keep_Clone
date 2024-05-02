bce63293ad820b854cc5e23ea93067805b6de523
murthasaali <murthasaalick123@gmail.com>
2024-05-02 16:26:16 +0530

    fea : size adjusted

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,13 +4,12 @@ import { CiSearch } from "react-icons/ci";
 
 function Navbar() {
   return (
-    <div className="w-full h-20  backdrop-blur-sm flex  justify-between px-10 items-center absolute z-[999]">
-      <div className="w-40 h-full flex justify-center text-3xl items-center ">
-        <p className="text-red-100">Note</p>
-      </div>
-      <div className="w-[250px] h-12 flex justify-start items-center overflow-hidden gap-2 px-3  rounded-full bg-stone-100 bg-opacity-40 ">
-        <CiSearch className="text-xl"/>
-        <input className="w-full h-10 bg-transparent outline-none"/>
+    <div className="w-full h-20  backdrop-blur-sm flex  justify-between md:px-10 px-3 items-center absolute z-[999]">
+      
+   
+      <div className="md:w-[250px] w-[200px] h-12 flex justify-start items-center overflow-hidden gap-2 px-3  rounded-full bg-stone-100 bg-opacity-40 ">
+        <CiSearch/>
+        <input className="w-full h-10 bg-transparent"/>
         
       </div>
       <div className="w-auto h-full flex items-center justify-center gap-2">
@@ -18,6 +17,7 @@ function Navbar() {
         <img src={profile} className="h-12 rounded-full w-12 "/>
 
       </div>
+
     </div>
   );
 }
